b35ee856a1041506ed44d978a3a42b252580fa09
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-01-27 14:29:59 +0530

    customize layout add global fonts, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,49 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from 'react'
- 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata = {
+  title: "CoderZHub",
+  description:
+    "CoderZHub is a community of developers for asking and answering programming questions. We are a community of like-minded people who want to learn, create, and grow together.",
+  icons: [],
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'text-primary-gradient hover:text-primary-600'
+        }
+      }}  
+    
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main>
-        <Navbar />
+        {/* <Navbar /> */}
     </main>
   )
 }
