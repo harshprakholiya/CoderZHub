973da7fa5f32046c73c99f7a7383c90ce3b4a7e0
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-02-29 23:41:45 +0530

    ...

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: [
       '/',
-      '/api/webhooks',
+      '/api/webhook',
       'questions/:id',
       '/tags',
       '/tags/:id',
