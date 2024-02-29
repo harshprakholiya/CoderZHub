fc33aa61a290985f6026360fb3c71aec9cb0dc1e
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-02-29 18:12:59 +0530

    Added clerk webhook, created actions for user creation, user update and user delete, sync clerk data with mongodb database

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,67 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
+
+
+export async function deleteUser(params: DeleteUserParams){
+    try {
+        connectToDatabase();
+        const { clerkId } = params;
+        const user = await User.findOneAndDelete({clerkId}) 
+
+        if(!user) {
+            throw new Error("User not found");
+        }
+
+        // Delete user from database
+        
+        const userQuestionIds = await Question.find({author: user._id}).distinct('_id')
+        
+        // Delete user Questions
+        
+        await Question.deleteMany({author: user._id});
+        // TODO: delete Questions, answers, comments, etc.
+
+        const deletedUser = await User.findByIdAndDelete(user._id);
+
+        return deletedUser;
+
+         
+    } catch (error) {
+        console.log(`Error deleting user: ${error}`);
+        throw error;
+    }
+}
+
+
+export async function updateUser(params: UpdateUserParams) {
+
+    try {
+        connectToDatabase();
+        const { clerkId, updateData, path } = params;
+        await User.findOneAndUpdate({clerkId}, updateData, {new: true});
+        revalidatePath(path);
+
+    } catch (error) {
+        console.log(`Error updating user: ${error}`)
+        throw error;
+    }
+}
+
+export async function createUser(userData: CreateUserParams) {
+    try {
+        connectToDatabase();
+        const newUser = new User(userData);
+        return newUser;
+
+    } catch (error) {
+       console.log(`Error creating user: ${error}`); 
+       throw error;
+    }
+}
 
 export async function getUserById(params: any) {
     try {
