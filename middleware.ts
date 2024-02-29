515f937706643909148a6f4fc9c2a7fe9bfd3124
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-02-29 21:13:22 +0530

    add public routes to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,18 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: [
         '/',
-    ],
+        '/api/webhooks',
+        'question/:id',
+        '/tags',
+        '/tags/:id',
+        '/profile/:id',
+        '/community',
+        '/jobs'
+      ],
+      ignoredRoutes: [
+        '/api/webhooks',
+        '/api/chatgpt'
+      ]
 });
  
 export const config = {
