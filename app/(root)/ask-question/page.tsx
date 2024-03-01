270c11d59ad5dc19bef7a6344f46e3982e8caf12
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-03-01 16:37:55 +0530

    remove static userId and add real userID in ask-question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,12 @@
 import Question from '@/components/forms/Question';
 import { getUserById } from '@/lib/actions/user.action';
-// import { auth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = 'user_2d5FpFOLqsdAjZ3K3vVVwLRH0Do';
+  const { userId } = auth();
+  // const userId = 'user_2d5FpFOLqsdAjZ3K3vVVwLRH0Do';
 
   if (!userId) redirect('/sign-in');
   const mongoUser = await getUserById({ userId });
