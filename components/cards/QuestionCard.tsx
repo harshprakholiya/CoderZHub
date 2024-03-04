2966e596c9ccc5fa1713dc9116b95b6787e73e0b
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-03-04 22:59:17 +0530

    remove plaseholder picture from question card and add actural author img

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -51,8 +51,8 @@ const QuestionCard = ({
       </div>
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          // imgUrl={author.picture}
-          imgUrl='/assets/icons/avatar.svg'
+          imgUrl={author.picture}
+          // imgUrl='/assets/icons/avatar.svg'
           alt="avatar"
           title={` - asked ${getTimeStamps(createdAt)}`}
           textStyles="body-medium card-text-invert-secondary"
