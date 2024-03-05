7b86ee245261839e4d59d47e04fe77cd8a220dd7
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-03-05 19:45:18 +0530

    added tags in questoin details page and creted form for answer

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Answer from '@/components/forms/Answer';
 import Metric from '@/components/shared/Metric';
 import ParseHtml from '@/components/shared/ParseHtml';
+import RenderTags from '@/components/shared/RenderTags';
 import { getQuestionById } from '@/lib/actions/question.action';
 import { formatNumber, getTimeStamps } from '@/lib/utils';
 import Image from 'next/image';
@@ -58,6 +60,19 @@ const Page = async ({ params, searchParams }: any) => {
       </div>
 
       <ParseHtml content={result.content} />
+
+      <div className="mt-8 flex flex-wrap gap-2">
+        {result.tags.map((tag: any) => (
+          <RenderTags
+            key={tag._id}
+            _id={tag._id}
+            name={tag.name}
+            showCount={false}
+          />
+        ))}
+      </div>
+
+      <Answer />
     </>
   );
 };
