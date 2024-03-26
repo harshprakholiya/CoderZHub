f0ea7b55f8fed4f8adf18a86429e761c4d48d34b
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-03-27 04:14:43 +0530

    created frontend for ask-ai page

--- app/(root)/ask-ai/page.tsx
+++ app/(root)/ask-ai/page.tsx
@@ -1,21 +1,24 @@
 'use client';
 
+import NoResult from '@/components/shared/NoResult';
 import ParseHtml from '@/components/shared/ParseHtml';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { ReloadIcon } from '@radix-ui/react-icons';
 import { useState } from 'react';
 
 const Page = () => {
   const [question, setQuestion] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [content, setContent] = useState('')
-
+  const [content, setContent] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const handelInputChange = (value: string) => {
     setQuestion(value);
   };
 
   const handleSubmit = async () => {
     setIsSubmitting(true);
+    setIsLoading(true);
 
     try {
       const response = await fetch(
@@ -31,36 +34,54 @@ const Page = () => {
       const aiReplay = aiAnswer.reply;
 
       setContent(aiReplay);
+      setQuestion('');
     } catch (error) {
       console.log(error);
       throw error;
     } finally {
       setIsSubmitting(false);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
-      <h1>Ask AI</h1>
-      <div>
-        {question === '' ? (
-          'Ask me anything...'
-        ) : (
-          <div>
-            <p>This is Question...</p>
-            <ParseHtml content={content} />
-          </div>
-        )}
-      </div>
-      <div>
+    <div className="flex flex-col">
+      <div className="mb-10 flex items-center justify-between gap-5 max-sm:w-full">
         <Input
           placeholder="Ask me anything..."
           type="text"
+          value={question}
           onChange={(e) => handelInputChange(e.target.value)}
+          className="paragraph-regular no-focus search_background min-h-[50px] border-none px-5 text-gray-600 shadow-none outline-none dark:text-gray-400"
         />
-        <Button disabled={isSubmitting} onClick={handleSubmit}>
-          Ask AI
+        <Button
+          disabled={isSubmitting}
+          onClick={handleSubmit}
+          className="btn primary-gradient body-medium rounded-md p-5 text-grey-50"
+        >
+          {
+            content === '' ? 'Ask AI' : 'clear'
+          }
         </Button>
+      </div>
+      <div className=" overflow-y-auto">
+        <div className="">
+          {content === '' && !isLoading ? (
+            <NoResult
+              title="How can i help you?"
+              description="ask me anything related to programming and i will try to answer you.😊"
+            />
+          ) : isLoading ? (
+            <div className="flex-center flex-col px-5">
+              <ReloadIcon className="mt-3 h-7 w-7 animate-spin text-primary-400" />
+              <p className="paragraph-regular text-invert-secondary mt-5">
+                Generating Answer...
+              </p>
+            </div>
+          ) : (
+            <ParseHtml content={content} />
+          )}
+        </div>
       </div>
     </div>
   );
--- app/api/ask-ai/route.ts
+++ app/api/ask-ai/route.ts
@@ -15,7 +15,7 @@ export const POST = async (request: Request) => {
         messages: [
           {
             role: 'system',
-            content: 'You are a experienced developer. when someone ask you a question you gives them replay within tags like h1, h2, p, div, span, etc. keep this things in mind : 1. make sure output of your answer is looking good 2. when you have to write code block you will remove "```...```" from code and add write code in <pre>{code block}</pre>< 3. you give only answers to questions related programming if question is not related programming then you simply says: "i am only able to answers related to programming'
+            content: 'You are a experienced developer. when someone ask you a question you gives them replay within tags like h1, h2, p, div, span, etc. keep this things in mind : 1. make sure output of your answer is looking good 2. when you have to write code block you will remove "```...```" from code and add write code in <pre>{code block}</pre>< 3. you give only answers to questions related programming. if answer is not related to programming simply decline to answer in fun tone. 4. you can use html tags to format your answer. '
           }, {
             role: 'user',
             content: `This is question answer it: ${question}`
