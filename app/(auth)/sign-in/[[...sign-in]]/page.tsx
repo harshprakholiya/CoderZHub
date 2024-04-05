e6c27b0fcc8b4de9fd9cefbb69f25cf033650cac
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-04-05 23:54:43 +0530

    metadata test 2

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,28 @@
-import { SignIn } from "@clerk/nextjs";
-import type { Metadata } from "next";
+import { SignIn } from '@clerk/nextjs';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Sign In",
-  description: "Sign in to your account",
   openGraph: {
-    title: "Sign In",
-    description: "Sign in to your account",
-    images: "https://www.clerk.dev/images/social.png"
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: '/assets/images-og-800X600.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/assets/images-og-1800X1600.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
-
 };
 
 export default function Page() {
