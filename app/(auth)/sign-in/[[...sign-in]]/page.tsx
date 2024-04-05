1c48a1cb22a5a2ce9690ad24a06b3a6a4e9220c2
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-04-05 23:22:18 +0530

    metadata test\

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,16 @@
 import { SignIn } from "@clerk/nextjs";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Sign in to your account",
+  openGraph: {
+    title: "Sign In",
+    description: "Sign in to your account",
+    images: "https://www.clerk.dev/images/social.png"
+  },
+
+};
 
 export default function Page() {
   return <SignIn />;
