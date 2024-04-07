28f51a19c8158ea3b7c432970b9dc1d1a5997d80
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2024-04-07 13:38:12 +0530

    installed framer motion

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -18,6 +18,8 @@ const Theme = () => {
     <div>
       <Menubar className="relative border-none bg-transparent shadow-none">
         <MenubarMenu>
+        
+
           <MenubarTrigger className="cursor-pointer">
             {mode === 'light' ? (
               <Image
@@ -37,6 +39,7 @@ const Theme = () => {
           </MenubarTrigger>
           <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[150px] border bg-white py-2 dark:border-none dark:bg-primaryDark-800">
             {themes.map((item) => (
+
               <MenubarItem
                 key={item.value}
                 className="flex cursor-pointer items-center gap-4 rounded-md px-2.5 py-2 transition-all duration-300 ease-in-out hover:bg-grey-50 dark:bg-primaryDark-800 dark:hover:bg-primaryDark-700"
@@ -48,25 +51,26 @@ const Theme = () => {
                     localStorage.removeItem('theme');
                   }
                 }}
-              >
+                >
                 <Image
                   src={
                     item.value === mode
-                      ? item.activeIcon
-                      : mode === 'dark'
-                        ? item.lightIcon
-                        : item.darkIcon
+                    ? item.activeIcon
+                    : mode === 'dark'
+                    ? item.lightIcon
+                    : item.darkIcon
                   }
                   alt={item.value}
                   width={16}
                   height={16}
-                />
+                  />
                 <p
                   className={`body-semibold text-primaryDark-100 ${mode === item.value ? 'primary-text-gradient' : mode === 'light' ? 'text-primaryDark-400' : 'text-primaryDark-100'} `}
-                >
+                  >
                   {item.label}
                 </p>
               </MenubarItem>
+
             ))}
           </MenubarContent>
         </MenubarMenu>
